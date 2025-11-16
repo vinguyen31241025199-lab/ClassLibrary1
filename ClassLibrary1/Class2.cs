ca923f2e6275bfcaaf7a2ee6fa18917194e9f8e3
Admin <Admin@KDZII-116>
2025-11-16 14:48:23 +0700

    Add project files.

---
+++ ClassLibrary1/Class2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Customer
+        {
+            public int AccountID { get; set; }
+            public string Adress { get; set; }
+            public DateTime Birthday { get; set; }
+            public int CustomerID { get; set; }
+            public string CustomerName { get; set; }
+            public string Email { get; set; }
+            public string PhoneNumber { get; set; }
+
+            // Methods
+            public Customer() { }
+
+            public void FindInfo()
+            {
+                // Some logic here
+            }
+
+            public void FindName()
+            {
+                // Some logic here
+            }
+
+            public void GetCustomers()
+            {
+                // Some logic here
+            }
+
+            public void Update()
+            {
+                // Some logic here
+            }
+        }
+}
---
+++ ClassLibrary1/Class3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Node
+    {
+        public int Data;
+        public Node LeftNode;
+        public Node RightNode;
+
+        public Node(int data)
+        {
+            Data = data;
+            LeftNode = null;
+            RightNode = null;
+        }
+    }
+}
